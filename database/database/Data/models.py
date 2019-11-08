65d55b89f19a65d02bb58447269db457fe21b7ac
Aniket Gupta <waytoaniketgupta@gmail.com>
2019-11-08 19:08:08 +0530

    Django-Database

--- database/database/Data/models.py
+++ database/database/Data/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 
 class Database(models.Model):
-    Fname = models.CharField(max_length=200)
-    Lname = models.CharField(max_length=200)
-    Email = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    Add = models.CharField(max_length=200)
-    Country = models.CharField(max_length=200)
-    Zip = models.CharField(max_length=6)
-    Phone = models.CharField(max_length=10)
-    Text = models.CharField(max_length=200)
+    id=models.CharField(max_length=10,primary_key=True,default="")
+    Fname = models.CharField(max_length=200,default="")
+    Lname = models.CharField(max_length=200,default="")
+    Email = models.CharField(max_length=200,default="")
+    city = models.CharField(max_length=200,default="")
+    Add = models.CharField(max_length=200,default="")
+    Country = models.CharField(max_length=200,default="")
+    Zip = models.CharField(max_length=6,default="")
+    Phone = models.CharField(max_length=10,default="")
+    Text = models.TextField(max_length=200,default="")
 
 
     def __str__(self):
